7d28bc37ca285c2bdb4a44445a1c7485dee72d61
adamUCDCS <adam.freeman@ucdconnect.ie>
2017-04-05 19:12:58 +0000

    Updated parser class

--- src/poker/Parser.java
+++ src/poker/Parser.java
@@ -1,5 +1,7 @@
 package poker;
 
+import java.util.Scanner;
+
 /**
  * Created by Adam Freeman on 05/04/2017.
  */
@@ -23,5 +25,39 @@ public class Parser {
         }
         return false;
     }
-    
+
+    public int[] convertDiscards(String response){
+        String [] strArray = response.split("\\s*(\\s|=>|,)\\s*");
+        int discard[] = new int[3];
+
+        for (int i = 0; i < discard.length; i++){
+
+            if(i < strArray.length)
+                discard[i] = Integer.parseInt(strArray[i]);
+            else
+                discard[i] = -1;
+
+        }
+        return discard;
+    }
+
+    public static void main(String[] args){
+        Parser parser = new Parser();
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter discard answer Y/N");
+        String response = input.nextLine();
+        System.out.println(parser.convertResponse(response));
+
+        System.out.println("Enter discard position");
+        String discard = input.nextLine();
+        int discards[] = parser.convertDiscards(discard);
+        for(int i = 0; i < discards.length; i++)
+            System.out.println(discards[i]);
+
+
+
+
+
+    }
 }
