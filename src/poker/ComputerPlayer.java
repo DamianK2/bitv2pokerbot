3aea0aa23aa8cbef4f5bb432c769759c7529eaab
DamianK2 <dkluziak303@gmail.com>
2017-04-09 13:05:35 +0100

    computer behaviour generator

--- src/poker/ComputerPlayer.java
+++ src/poker/ComputerPlayer.java
@@ -4,25 +4,45 @@ import java.util.Random;
 
 public class ComputerPlayer extends PokerPlayer {
 
-    public ComputerPlayer(DeckOfCards deck)
-    {
+	private String behaviourType;
+	private static final int NO_OF_NAMES = 20, NO_OF_BEHAVIOURS = 3;
+	
+	public ComputerPlayer(DeckOfCards deck) {
         super(deck);
         this.name = this.getRandomName();
+        this.behaviourType = this.getRandomBehaviour();
         this.isHuman = false;
     }
 
-    private String getRandomName()
-    {
-        Random rand = new Random();
-        int n = rand.nextInt(20);
+    private String getRandomName() {
+        int position = this.generateRandomNumber(ComputerPlayer.NO_OF_NAMES);
         String[] names = {"Brittanie", "Jim", "Lilli", "Daysi", "Elouise", "Horace", "Corazon", "Agustin", "Magan", "Vivan", "Retta", "Normand", "Rory", "Erick", "Antonio", "Gisele", "Parker", "Marla", "Cassie", "Aurelia"};
-
-        return names[n];
+        return names[position];
+    }
+    
+    private String getRandomBehaviour() {
+    	int position = this.generateRandomNumber(ComputerPlayer.NO_OF_BEHAVIOURS);
+    	String [] behaviours = {"risky", "normal", "no risk"};
+    	return behaviours[position];
+    }
+    
+    private int generateRandomNumber(int range) {
+    	Random rand = new Random();
+    	int number = rand.nextInt(range);
+    	return number;
     }
 
     public static void main(String[] args) {
         DeckOfCards deck = new DeckOfCards();
         ComputerPlayer p1 = new ComputerPlayer(deck);
-        System.out.print(p1.getName());
+        System.out.println(p1.getName());
+        if(p1.behaviourType == "risky") 
+        	System.out.println("risky");
+        else if(p1.behaviourType == "normal")
+        	System.out.println("normal");
+        else if(p1.behaviourType == "no risk") {
+        	System.out.println("not risky");
+        }
+        
     }
 }
