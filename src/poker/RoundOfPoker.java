5af76d92c94cb0b068400872ca01e43f2ef1bfd9
adamUCDCS <adam.freeman@ucdconnect.ie>
2017-04-24 15:25:35 +0000

    Clean up

--- src/poker/RoundOfPoker.java
+++ src/poker/RoundOfPoker.java
@@ -21,24 +21,24 @@ public class RoundOfPoker {
         game.updateGameMessage("New Deal:");
 
         for (PokerPlayer player : this.players)
-            game.updateGameMessage("> " + player.getName() + " has " + player.getCoinsBalance() + " coins in the bank");
+            game.updateGameMessage(player.getName() + " has " + player.getCoinsBalance() + " coins");
 
         // CHECK IF ANY PLAYER CAN OPEN
         boolean canOpen = false;
         for (PokerPlayer player : this.players)
             if (player.canOpenBet()) {
-                game.updateGameMessage("> " + player.getName() + " says: I can open");
+                game.updateGameMessage(player.getName() + ": I can open");
                 canOpen = true;
             }
             else
-                game.updateGameMessage("> " + player.getName() + " says: I cannot open");
+                game.updateGameMessage(player.getName() + ": I cannot open");
 
         if (!canOpen) {
-            game.updateGameMessage("Sorry, we cannot open the game.");
+            game.updateGameMessage("Cannot open game.");
             return;
         }
 
-        game.updateGameMessage("You have been dealt the following hand:");
+        game.updateGameMessage("You hand is:");
         // PRINT THE TYPE OF HAND THAT HUMAN PLAYER OWNS
         for (PokerPlayer player : this.players)
             if (player.isHuman())
@@ -47,7 +47,7 @@ public class RoundOfPoker {
         // DISCARD
         for (PokerPlayer player : this.players) {
             if (player.isHuman()) {
-                game.updateGameMessage(">> Which card(s) would you like to discard (e.g., 1,3): ");
+                game.updateGameMessage("Which card(s) would you like to discard (e.g., 1,3): ");
                 player.askDiscard();
             }
         }
@@ -55,7 +55,7 @@ public class RoundOfPoker {
         // PRINT THE TYPE OF HAND THAT HUMAN PLAYER OWNS
         for (PokerPlayer player : this.players)
             if (player.isHuman()) {
-                game.updateGameMessage("Your hand now looks like:");
+                game.updateGameMessage("Your new hand:");
                 game.updateGameMessage(player.getHand());
             }
 
@@ -65,7 +65,7 @@ public class RoundOfPoker {
         for (int i = 0; i < players.size(); i++) {
 
             if (players.get(i).isHuman()) {
-                game.updateGameMessage(">> Would you like to fold (y/n)? ");
+                game.updateGameMessage("Would you like to fold (y/n)? ");
             }
             fold[i] = players.get(i).askFold(this.currentBet);
             if(!fold[i])
@@ -74,13 +74,13 @@ public class RoundOfPoker {
         // SHOW DISCARDING STATS
         for (PokerPlayer player : this.players)
             if (!player.isHuman())
-                System.out.println(player.getName() + " discards " + player.discard() + " card(s)");
+                game.updateGameMessage(player.getName() + " discards " + player.discard() + " card(s)");
 
         System.out.println("");
 
         // IF EVERY PLAYER FOLD THEN EXIT ROUND OF POKER
         if (!allFold) {
-            System.out.println("sorry, all players fold in the round.");
+            game.updateGameMessage("sorry, all players fold in the round.");
             return;
         }
 
@@ -99,7 +99,7 @@ public class RoundOfPoker {
                         if(players.get(i).isHuman()){
                             // CHECK IF THE HUMAN PLAYER IS THE FIRST PLAYER TO OPEN
                             if (players.get(i).canOpenBet() && !fold[i] && human && checkOpen == 0) {
-                                System.out.println("Would you like to open bet (y/n)? ");
+                                game.updateGameMessage("Would you like to open bet (y/n)? ");
                                firstOpen = players.get(i).askOpenBet(this.currentBet);
                                if(firstOpen){
                                    this.currentBet = 1;
@@ -123,7 +123,7 @@ public class RoundOfPoker {
 
                     // CHECK IF THIS IS THE FIRST TIME OF OPENING AND PRINT THE OPENING STATEMENT
                     if (checkOpen == 0 && firstOpen) {
-                        System.out.println(players.get(i).getName() + " says: I open with " + this.currentBet + " chip!");
+                        game.updateGameMessage(players.get(i).getName() + ": I open with " + this.currentBet + " chip!");
                         currentPot += this.currentBet;
                         checkOpen = 1;
                     }
@@ -137,7 +137,7 @@ public class RoundOfPoker {
 
                             printSeenStatement(currentPot, i);
 
-                            System.out.println("Would you like to raise (y/n)? ");
+                            game.updateGameMessage("Would you like to raise (y/n)? ");
                             boolean checkHuman = players.get(i).askRaiseBet(this.currentBet);
 
                             // IF THE PLAYER SAID YES THEN RAISE BET
@@ -155,7 +155,7 @@ public class RoundOfPoker {
                             else if (!checkHuman ) {
                                //System.out.println("Would you like to fold (y/n)? ");
                                 fold[i] = true;
-                                System.out.println(players.get(i).getName() + " says: I fold ");
+                                game.updateGameMessage(players.get(i).getName() + ": I fold ");
                             }
                         }
                         // CHECK IF THE PLAYER IS A COMPUTER PLAYER AND ASK THE PLAYER TO RAISE THE BET
@@ -183,7 +183,7 @@ public class RoundOfPoker {
                             // CHECK IF THE PLAYER DIDN'T RAISE THE BET AND THE BETTING ISN'T THE OPENING BET THEN FOLD
                             else if (!checkComputer) {
                                 fold[i] = true;
-                                System.out.println(players.get(i).getName() + " says: I  fold ");
+                                game.updateGameMessage(players.get(i).getName() + ": I  fold ");
                             }
                         }
                     }
@@ -316,7 +316,7 @@ public class RoundOfPoker {
 
         boolean poker = true;
         while(poker /**&& players.contains(humanPlayer)*/){
-            round.play();
+            //round.play();
             System.out.println("Would like to play another round of poker (y/n)");
             Scanner in = new Scanner(System.in);
             String response = in.nextLine();
